52131f3fa2ade6f9a9d7cad3b914e1d1f339d039
yesidlopez <PI2@hgrid9.icesi.edu.co>
2018-03-26 14:01:38 -0500

    sdadsa

--- src/pruebaGit/Sumar.java
+++ src/pruebaGit/Sumar.java
@@ -1,7 +1,11 @@
 package pruebaGit;
 
 public class Sumar {
-		public static int suma(int a, int b) {
-			return a+b;
-		}
+	public static int suma(int a, int b) {
+		return a + b;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(suma(2, 3));
+	}
 }
